70c26cf3939b0cf1f4e065b3a933c996481653d6
Pau Rovira <paro116@vidalibarraquer.net>
2026-09-25 08:16:22 +0200

    RectangleQuadrat

--- src/main/java/AccesPerHora.java
+++ src/main/java/AccesPerHora.java
@@ -2,8 +2,22 @@
 // Ajuda: fes servir java.util.Calendar per saber l'hora actual
 //   Calendar calendar = Calendar.getInstance();
 //   int hour = calendar.get(Calendar.HOUR_OF_DAY);
+
+import java.util.Calendar;
+
 public class AccesPerHora {
     public static void main(String[] args) {
         // TODO: mostra "Pots accedir" únicament si ja han passat les 8 del matí
+
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+
+        if (hour > 8) {
+
+            System.out.println("Pots accedir");
+            
+        }
+
+
     }
 }
--- src/main/java/RectangleQuadrat.java
+++ src/main/java/RectangleQuadrat.java
@@ -1,8 +1,32 @@
 // Activitat 06 — Rectangle o quadrat
+import java.util.Scanner;
 public class RectangleQuadrat {
     public static void main(String[] args) {
         // TODO: llegeix el costat gran i el costat petit d'un rectangle
         //   Mostra el perímetre (costatGran*2 + costatPetit*2) i l'àrea (costatGran*costatPetit)
         //   Digues si és un quadrat (els dos costats iguals) o no
+
+        Scanner teclat = new Scanner (System.in);
+       System.out.println("Entra el valor del costat gran del rectangle:");
+       double costatGran = teclat.nextInt();
+
+       System.out.println("Entra el valor del costat petit del rectangle:");
+       double costatPetit = teclat.nextInt();
+
+       double area = costatGran*costatPetit;
+       double perimetre = costatGran*2 + costatPetit*2;
+
+       System.out.println("Perimetre del rectangle = " + perimetre);
+       System.out.println("Àrea del rectangle = " + area);
+
+       if (costatGran == costatPetit) {
+        System.out.println("Es un quadrat!");
+           
+       }
+       else{
+        System.out.println("No es un quadrat");
+       }
+
+       
     }
 }
