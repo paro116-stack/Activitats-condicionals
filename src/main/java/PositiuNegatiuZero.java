7eb13884560e2823f2f80895181efe315c89970a
Pau Rovira <paro116@vidalibarraquer.net>
2026-09-25 08:33:21 +0200

    PositiuNegatiuZero

--- src/main/java/PositiuNegatiuZero.java
+++ src/main/java/PositiuNegatiuZero.java
@@ -1,6 +1,22 @@
 // Activitat 08 — Positiu, negatiu o zero
+import java.util.Scanner;
 public class PositiuNegatiuZero {
     public static void main(String[] args) {
         // TODO: llegeix un número enter i digues si és positiu, negatiu o zero
+        
+        Scanner teclat = new Scanner(System.in);
+        System.out.println("Entra un numero enter:");
+        int enter = teclat.nextInt();
+
+        if (enter>0) {
+            System.out.println("El numero es positiu!");   
+        }
+        if (enter<0) {
+            System.out.println("El número és negatiu");   
+        }  
+        if (enter == 0) {
+            System.out.println("Es zero");
+        }   
+
     }
 }
